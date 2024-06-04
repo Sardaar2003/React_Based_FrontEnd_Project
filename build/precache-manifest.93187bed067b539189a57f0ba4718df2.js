a3bdf2ddc7a23c9ef05f3ace93de0f8cfb0a04fb
Sardaar2003 <singhmantej536@gmail.com>
2024-06-04 19:04:07 +0530

    Changes in Build Program

---
+++ build/precache-manifest.93187bed067b539189a57f0ba4718df2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9019f1cef43cb1245f7a38411b00bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "584a9e5adf91d9099468",
+    "url": "/static/css/main.780122e2.chunk.css"
+  },
+  {
+    "revision": "d34320712587be62b855",
+    "url": "/static/js/2.f5d8afee.chunk.js"
+  },
+  {
+    "revision": "1f34240408b6a3434ea78948441340d0",
+    "url": "/static/js/2.f5d8afee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584a9e5adf91d9099468",
+    "url": "/static/js/main.970b6025.chunk.js"
+  },
+  {
+    "revision": "4c4ce1ce762150c091d3",
+    "url": "/static/js/runtime-main.c33b729d.js"
+  },
+  {
+    "revision": "d6b9a6e67086d81abeb2bab4d63a0b82",
+    "url": "/static/media/bg1.d6b9a6e6.jpg"
+  },
+  {
+    "revision": "54685a86c72f668e280efe20675d8201",
+    "url": "/static/media/logo192.54685a86.png"
+  }
+]);
