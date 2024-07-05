2e17d214c5d82dd4e1fc2eafd363e0a60e875acb
Sardaar2003 <singhmantej536@gmail.com>
2024-07-05 15:34:11 +0530

    Build Folder

---
+++ build/precache-manifest.7e70bb63d57dfadae1470ecf91f9fc3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44da27abfca4b11492242a83f04ca60e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60b74431414f9f152c3",
+    "url": "/static/css/main.89f10bf2.chunk.css"
+  },
+  {
+    "revision": "843b00c63d9da316f2b4",
+    "url": "/static/js/2.6416b094.chunk.js"
+  },
+  {
+    "revision": "1f34240408b6a3434ea78948441340d0",
+    "url": "/static/js/2.6416b094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60b74431414f9f152c3",
+    "url": "/static/js/main.e8543f30.chunk.js"
+  },
+  {
+    "revision": "4c4ce1ce762150c091d3",
+    "url": "/static/js/runtime-main.c33b729d.js"
+  },
+  {
+    "revision": "d6b9a6e67086d81abeb2bab4d63a0b82",
+    "url": "/static/media/bg1.d6b9a6e6.jpg"
+  },
+  {
+    "revision": "54685a86c72f668e280efe20675d8201",
+    "url": "/static/media/logo192.54685a86.png"
+  }
+]);
